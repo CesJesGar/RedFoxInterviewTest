fe9da8a3650a9b7d317adfc08faf32a3a40d62bc
CesJesGar <cesarjgarciaa@outlook.com>
2025-07-22 21:32:26 -0300

    Add Address ID to user entity

--- RedFox.Domain/Entities/User.cs
+++ RedFox.Domain/Entities/User.cs
@@ -1,16 +1,17 @@
-namespace RedFox.Domain.Entities;
+ namespace RedFox.Domain.Entities;
 
-public class User
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
+ public class User
+ {
+     public int Id { get; set; }
 
-    public string Email { get; set; } = string.Empty;
+     public string Email { get; set; } = string.Empty;
+     public string Name { get; set; } = string.Empty;
+     public string Username { get; set; } = string.Empty;
+     public string Phone { get; set; } = string.Empty;
+     public string Website { get; set; } = string.Empty;
 
-    public string Phone { get; set; } = string.Empty;
-
-    public string Website { get; set; } = string.Empty;
-    public int CompanyId { get; set; }
-    public Company? Company { get; set; }
-}
+     public int CompanyId { get; set; }
+     public Company? Company { get; set; }
++    public int AddressId { get; set; }
++    public Address? Address { get; set; }
+ }
