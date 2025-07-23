f13ef30f9dbcb7be372c2a6fa9cb14d3a0a6b5a7
CesJesGar <cesarjgarciaa@outlook.com>
2025-07-22 21:40:58 -0300

    Configure relations and add address and geo

--- RedFox.Infrastructure/AppDbContext.cs
+++ RedFox.Infrastructure/AppDbContext.cs
@@ -9,15 +9,32 @@ using RedFox.Domain.Entities;
 
 namespace RedFox.Infrastructure;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options), IAppDbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : DbContext(options), IAppDbContext
 {
-    public DbSet<User> Users => Set<User>();
-
-    public DbSet<Company> Companies => Set<Company>();
+    public DbSet<User>    Users      => Set<User>();
+    public DbSet<Company> Companies  => Set<Company>();
+    public DbSet<Address> Addresses  => Set<Address>();
+    public DbSet<Geo>     Geos       => Set<Geo>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        // UserAddress relation
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Address)
+            .WithOne()
+            .HasForeignKey<User>(u => u.AddressId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        //AddressGeo relation
+        modelBuilder.Entity<Address>()
+            .HasOne(a => a.Geo)
+            .WithOne()
+            .HasForeignKey<Address>(a => a.GeoId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
