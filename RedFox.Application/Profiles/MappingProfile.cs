e1309ac7085a5673ecbbffa259946e74dd588111
CesJesGar <cesarjgarciaa@outlook.com>
2025-07-23 21:21:37 -0300

    Refine MappingProfile: ignore IDs & configure nested mappings
    Ignore Id on reverse maps for Geo, Address, and Company
    
    Ensure Address.Geo and Company are mapped correctly
    
    Add and adjust mappings for UserCreationDto and UserUpdateDto to/from User

--- RedFox.Application/Profiles/MappingProfile.cs
+++ RedFox.Application/Profiles/MappingProfile.cs
@@ -8,20 +8,25 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<Geo, GeoDto>().ReverseMap();
-        CreateMap<Address, AddressDto>().ReverseMap();
-        CreateMap<Company, CompanyDto>().ReverseMap();
+        CreateMap<Geo, GeoDto>().ReverseMap()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+        CreateMap<Address, AddressDto>().ReverseMap()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Geo, opt => opt.MapFrom(src => src.geo));
+
+        CreateMap<Company, CompanyDto>().ReverseMap()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+
         CreateMap<User, UserDto>();
+
         CreateMap<UserCreationDto, User>()
-            .ForMember(dest => dest.Company,
-                       opt => opt.MapFrom(src => src.Company))
-            .ForMember(dest => dest.Address,
-                       opt => opt.MapFrom(src => src.Address));
+            .ForMember(dest => dest.Company, opt => opt.MapFrom(src => src.Company))
+            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address));
 
         CreateMap<UserUpdateDto, User>()
-            .ForMember(dest => dest.Company,
-                       opt => opt.MapFrom(src => src.Company))
-            .ForMember(dest => dest.Address,
-                       opt => opt.MapFrom(src => src.Address));
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Company, opt => opt.MapFrom(src => src.Company))
+            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address));
     }
 }
